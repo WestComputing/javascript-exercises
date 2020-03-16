3845dbeb1156ff94d64b01a546bf5e33f13ad192
Julian West <west221b@gmail.com>
2020-03-16 15:08:46 -0400

    Add caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,21 @@
-const caesar = function() {
+const caesar = (text, rotate) => text.split('').map(char => {
 
-}
+      const alphaSize = 26;
+      let code = char.charCodeAt(0);
+      let cipher = code + (rotate % alphaSize);
+
+      if (code > 64 && code < 91) {
+        if (cipher > 90) cipher -= alphaSize;
+        if (cipher < 65) cipher += alphaSize;
+        char = String.fromCharCode(cipher);
+      } else if (code > 96 && code < 123) {
+        if (cipher > 122) cipher -= alphaSize;
+        if (cipher < 97) cipher += alphaSize;
+        char = String.fromCharCode(cipher);
+      }
+
+      return char;
+
+    }).join('');
 
 module.exports = caesar
