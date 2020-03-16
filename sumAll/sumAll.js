4ea0e31740cae94d7339cd08e7f5ac2334785760
Julian West <west221b@gmail.com>
2020-03-16 00:22:11 -0400

    Add sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+
+  if ([...arguments].every(e => typeof e === 'number' && e >= 0)) {
+
+    let sum = 0;
+    for (let n = Math.min(...arguments); n <= Math.max(...arguments); n++) {
+      sum += n;
+    }
+    return sum;
+
+  } else {
+
+    return "ERROR";
+
+  }
 
 }
 
