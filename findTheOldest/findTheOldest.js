a2e923bdd0fce50116852d051536b4db317c7be0
Julian West <west221b@gmail.com>
2020-03-16 21:40:29 -0400

    Add findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-let findTheOldest = function() {
-
-}
+const findTheOldest = people => {
+  const now = new Date;
+  const thisYear = now.getFullYear();
+  return people.reduce((oldest, person) => {
+    const age = person => (person.yearOfDeath || thisYear) - person.yearOfBirth;
+    return age(person) > age(oldest) ? person : oldest;
+  });
+};
 
 module.exports = findTheOldest
