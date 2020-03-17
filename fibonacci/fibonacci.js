9a10d733c5617fd86c3fb2f329549be673cb01de
Julian West <west221b@gmail.com>
2020-03-16 21:03:09 -0400

    Add fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = n => {
+  if (typeof n === 'string') n = parseInt(n);
+  if (n < 1) return "OOPS";
+  if (n < 3) return 1;
+  let previousFibonacci = 1;
+  let currentFibonacci = 1;
+  for (let i = 3; i <= n; i++) {
+    [currentFibonacci, previousFibonacci] = [previousFibonacci + currentFibonacci, currentFibonacci];
+  }
+  return currentFibonacci;
 }
 
 module.exports = fibonacci
